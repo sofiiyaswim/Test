89e3dee65fa09cc1bb9cfa01861b664d1ba7eb0c
София Ю. Волошина <sofiyaswim@main.sgu.ru>
2021-03-24 13:18:14 +0400

    Test1

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using sueta.Properties;
+using sueta;
+using Microsoft.Win32;
+using System.IO;
+
+namespace sueta
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SearchCombo.ItemsSource = comboList;
+            SearchCombo.SelectedIndex = 0;
+        }
+
+        List<string> comboList = new List<string> { "Linear", "KMP", "BM" };
+
+        private string OpenUri { get; set; }
+        private string SaveUri { get; set; }
+        private string Text { get; set; }
+
+        private void OpenButt_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "Text files .txt|*.txt";
+
+            if (fileDialog.ShowDialog() == true)
+            {
+                this.OpenUri = fileDialog.FileName;
+
+                using (StreamReader sr = File.OpenText(this.OpenUri))
+                {
+                    this.Text = sr.ReadToEnd();
+                    InfomationTextBlock.Text = this.Text;
+                }
+            }
+        }
+
+
+
+        private void SaveButt_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "Text files .txt|*.txt";
+
+            if (fileDialog.ShowDialog() == true)
+            {
+                this.SaveUri = fileDialog.FileName;
+
+                using (StreamWriter sw = File.CreateText(this.SaveUri))
+                {
+                    int index = -1;
+                    if (SearchCombo.SelectedIndex == 0)
+                    {
+                        index = Search.LinearSearch(SubStringTextBox.Text, this.Text);
+                        MessageBox.Show((string)SearchCombo.SelectedItem);
+                    }
+                    else if(SearchCombo.SelectedIndex == 1)
+                    {
+                        index = Search.KMPSearch(SubStringTextBox.Text, this.Text);
+                        MessageBox.Show("KMP");
+                    }
+                    else if(SearchCombo.SelectedIndex == 2)
+                    {
+                        index = Search.BMSearch(SubStringTextBox.Text, this.Text);
+                        MessageBox.Show("BM");
+                    }
+                    sw.WriteLine(index);
+                }
+            }
+        }
+    }
+}
---
+++ Search.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sueta
+{
+    public static class Search
+    {
+        public static int LinearSearch(string SubValue, string Value)
+        {
+            int result = -1;//Если вхождение подстроки не найдено
+            for (int i = 0; i < Value.Length - SubValue.Length + 1; i++)
+            {
+                for (int j = 0; j < SubValue.Length; j++)
+                {
+                    if (SubValue[j] != Value[i + j])
+                    {
+                        break;
+                    }
+                    else if (j == SubValue.Length - 1)
+                    {
+                        return i;
+                    }
+                }
+            }
+            return result;
+        }
+
+        public static int BMSearch(string SubValue, string Value)
+        {
+            int sl, ssl;
+            int result = -1;
+            sl = Value.Length;
+            ssl = SubValue.Length;
+
+            int i, Pos;
+            int[] BMT = new int[256];
+            for (i = 0; i < 256; i++)
+                BMT[i] = ssl;
+            for (i = ssl - 1; i >= 0; i--)
+                if (BMT[(short)(SubValue[i])] == ssl)
+                    BMT[(short)(SubValue[i])] = ssl - i - 1;
+            Pos = ssl - 1;
+            while (Pos < sl)
+                if (SubValue[ssl - 1] != Value[Pos])
+                    Pos = Pos + BMT[(short)(Value[Pos])];
+                else
+                    for (i = ssl - 2; i >= 0; i--)
+                    {
+                        if (SubValue[i] != Value[Pos - ssl + i + 1])
+                        {
+                            Pos += BMT[(short)(Value[Pos - ssl + i + 1])] - 1;
+                            break;
+                        }
+                        else
+                          if (i == 0)
+                            return Pos - ssl + 1;
+                    }
+            return result;
+        }
+
+        public static int KMPSearch(string SubValue, string Value)
+        {
+           
+            int[] pf = GetPrefix(SubValue);
+            int index = 0;
+
+            for (int i = 0; i < Value.Length; i++)
+            {
+                while (index > 0 && SubValue[index] != Value[i])
+                {
+                    index = pf[index - 1];
+                }
+                if (SubValue[index] == Value[i]) index++;
+                if (index == SubValue.Length)
+                {
+                    return i - index + 1;
+                }
+            }
+            return -1;
+        }
+
+        public static int[] GetPrefix(string s)
+        {
+            int[] result = new int[s.Length];
+            result[0] = 0;
+            int index = 0;
+
+            for (int i = 1; i < s.Length; i++)
+            {
+                while (index >= 0 && s[index] != s[i]) { index--; }
+                index++;
+                result[i] = index;
+            }
+
+            return result;
+        }
+    }
+}
